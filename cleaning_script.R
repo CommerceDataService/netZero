d96650922035da5a2f1847426c281280e74284b6
Pri Oberoi <poberoi@doc.gov>
2016-08-05 15:02:25 -0400

    updated script to reshape data and output complete csv and well as a csv for each subsystem

--- cleaning_script.R
+++ cleaning_script.R
@@ -1,46 +1,71 @@
 library(plyr)
+ptm <- proc.time()
+
+###############
+# working directories
+###############
+
+# run convert_tdms_to_csv.py first, to convert tdms files into csv
+
+mainWD <- "/Users/prioberoi/Documents/nist/netZero"
+rawDataWD <- paste0(mainWD, "/data/raw")
+processedDataWD <- paste0(mainWD, "/data/processed")
+
+###############
+# import data
+###############
 
 # Names of CSVs to be imported
-paths <- dir(path = "/Users/prioberoi/Documents/nist/netZero/data/raw", pattern = "2015-02-[0-9]{2}\\.csv$", full.names = TRUE)
+#paths <- dir(path = rawDataWD, pattern = "2015-02-[0-9]{2}\\.csv$", full.names = TRUE)
+paths <- dir(path = rawDataWD, pattern = "\\.csv$", full.names = TRUE)
 names(paths) <- basename(paths)
 
 # use plyr to combine into one dataframe
-data <- ldply(.data = paths[1:20], 
+data <- ldply(.data = paths, 
               .fun = function(x){
                 read.csv(x, header = TRUE, stringsAsFactors = FALSE)
               },
               .progress = "time")
 
-# get a dataframe of csv sizes
-ptm <- proc.time()
-dims <- data.frame()
-for(path in paths){
-  temp <- read.csv(path, stringsAsFactors = FALSE)
-  dims <- rbind(dims, data.frame(file = path, rows = dim(temp)[1], columns = dim(temp)[2]))
+###############
+# variable name clean up
+###############
+# remove .
+names(data) <- gsub("\\.", "", names(data))
+# remove X prefix
+names(data)[3:ncol(data)] <- sub("X", "", names(data)[3:ncol(data)])
+
+# delineate between subsystem and channel
+dataDic <- read.csv(paste0(processedDataWD, "/dataDictionary.csv"))
+subsystems <- as.character(unique(dataDic$Subsystem[grep("[A-Z]+", dataDic$Subsystem)]))
+subsystems <- c(subsystems, "ElecCurrent", "ElecEnergy","ElecPower", "ElecRPB", "ElecTotal", "Load", "Misc", "Vent")
+for(subsystem in subsystems){
+  names(data) <- sub(paste0("^",subsystem), paste0(subsystem,"_"), names(data), ignore.case = FALSE)
 }
+
+###############
+# date parsing
+###############
+
+data <- data[,!(names(data) %in% c("TimeStampSchedulerTime", "TimeStampSystemTime"))] 
+data$TimeStampGPSTime <- strptime(substr(data$TimeStampGPSTime, 1, 19), "%Y-%m-%d %H:%M:%S")
+# create variable for day of week
+data$DayOfWeek <- as.factor(weekdays(data$TimeStampGPSTime))
+# store list of time variables
+timeVars <- c("TimeStampGPSTime", "DayOfWeek", "TimeStampCount")
+
+###############
+# write final CSVs to processed folder
+###############
+
+# write entire dataset
+write.csv(data, file = paste0(processedDataWD, "/data.csv"), row.names = FALSE)
+
+# write CSV for each subsystem
+for(subsystem in subsystems){
+  vars <- c(timeVars, names(data)[grep(paste0("^",subsystem), names(data))])
+  data_subsystem <- data[,vars]
+  write.csv(data_subsystem, file = paste0(processedDataWD, "/", subsystem, ".csv"), row.names = FALSE)
+}
+
 proc.time() - ptm
-
-checkVariables <- function(df1, df2){
-  if(sum(!(names(df1) %in% names(df2))) > 0){
-    add <- matrix(0, nrow = nrow(df2), ncol = sum(!(names(df1) %in% names(df2))))
-    add <- as.data.frame(add)
-    names(add) <- names(df1)[!(names(df1) %in% names(df2))]
-    df2 <- cbind(df2, add)
-  }
-  return(df2)
-}
-
-# combine csvs into one dataframe
-#paths <- c(as.character(dims$file[dims$columns == 913][1]), as.character(dims$file[dims$columns == 917][1]))
-ptm <- proc.time()
-data <- read.csv(paths[1], stringsAsFactors = FALSE)
-data$date <- paths[1]
-for(path in paths[2:length(paths)]){
-  temp <- read.csv(path, stringsAsFactors = FALSE)
-  temp$date <- path
-  data <- checkVariables(temp, data)
-  temp <- checkVariables(data, temp)
-  data <- rbind(data, temp)
-}
-proc.time() - ptm
-
