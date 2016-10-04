4f47ca1706dcdf3c8734a03131f6c00037a6f020
Pri Oberoi <poberoi@doc.gov>
2016-10-04 14:19:17 -0400

    added comments to python script to convert TDMS to CSV

--- convert_tdms_to_csv.py
+++ convert_tdms_to_csv.py
@@ -3,7 +3,8 @@
 from nptdms import TdmsFile
 import glob
 
-for filename in glob.iglob('/Volumes/NO NAME/*/*.tdms'):
+for filename in glob.iglob('/Volumes/NO NAME/*/*.tdms'): #update filepath to location with .tdms files from LabView
     tdms_file = TdmsFile(filename)
     temp = tdms_file.as_dataframe(time_index=False, absolute_time=False)
+    #update path below to save .csv files to a location of choice
     temp.to_csv(path_or_buf="/Users/prioberoi/Documents/nist/netZero/data/raw/"+filename[28:-13]+".csv", encoding='utf-8')
