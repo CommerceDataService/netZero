28c7f9af02f1df729890f27a51834e66eb15f926
Pri Oberoi <poberoi@doc.gov>
2016-11-07 09:17:15 -0500

    comments on cleaning script, removed datadic from processed dir (it's in user_updates), added most recent metadata (with hourly aggregates column), added csv where the user can define new variable names

--- cleaning_script.R
+++ cleaning_script.R
@@ -1,4 +1,7 @@
+################################################################################################################
+# This code cleans and preprocesses labview and alternate system data 
 # Be sure to run the convert_tdms_to_csv.py first, to convert TDMS files into CSVs
+################################################################################################################
 
 ###############
 # working directories
@@ -37,18 +40,23 @@ for(path in paths){
   day$X <- NULL
   names(day) <- sub("^X", "", names(day))
   # format timestamp as object of class POSIXlt
-  day$TimeStamp_SystemTime <- strptime(substr(day$TimeStamp_SystemTime, 1, 19), "%Y-%m-%d %H:%M:%S", tz = "EST")
+  day$TimeStamp_SystemTime <- strptime(substr(day$TimeStamp_SystemTime, 1, 19), "%Y-%m-%d %H:%M:%S", tz = "GMT")
+  day$TimeStamp_SystemTime <- as.POSIXct(day$TimeStamp_SystemTime)
+  # set to EST instead of GMT
+  attr(day$TimeStamp_SystemTime, "tzone") <- "EST"
   # create new timestamp rounded to the minute
   day$Timestamp <- round(day$TimeStamp_SystemTime, units = "min")
   day <- day[!(duplicated(day$Timestamp)),]
+  # Remove extraneous timestamp variables, like TimeStamp_GPSTime, TimeStamp_SystemTime, TimeStamp_SchedulerTime
+  day <- day[,!(names(day) %in% c("TimeStamp_GPSTime", "TimeStamp_SystemTime", "TimeStamp_SchedulerTime"))]
   # add any new channels (columns) from day to d
   temp <- as.data.frame(matrix(NA, ncol = length(names(day)[!(names(day) %in% names(d))]), nrow = nrow(d)))
   names(temp) <- names(day)[!(names(day) %in% names(d))]
   d <- cbind(d, temp)
-  # add data to channels
-  vars <- names(day)
-  vars <- vars[!(vars %in% vars[grep("^Time", vars)])]
-  d[d$Timestamp %in% day$Timestamp,vars] <- day[,vars]
+  # list of all variables that have data that need to be added to d from day
+  vars <- names(day)[!(names(day) %in% grep("^Time", names(day), value = TRUE))]
+  # add data to channels where timestamp matches
+  d[as.character(d$Timestamp) %in% as.character(day$Timestamp),vars] <- day[as.character(day$Timestamp) %in% as.character(d$Timestamp),vars]
 }
 proc.time() - ptm
 
@@ -57,8 +65,7 @@ proc.time() - ptm
 ############### 
 
 ### only run the code below if you are also adding data from an alternate instrumentation system that uses txt files ###
-### note that this code runs after the code above which removes any unwanted variables ###
-### additionally, update the string below to indicate which subsystem these channels belong to ###
+### remember to update the string below to indicate which subsystem these new channels belong to ###
 altSubSystem <- "HVAC"
 
 # import data from alternate instrument acquisition system, add new channels (columns) as needed, and add data to existing channels 
@@ -84,8 +91,8 @@ for(path in paths){
   d <- cbind(d, temp)
   # add data to channels
   vars <- names(day)
-  vars <- vars[!(vars %in% vars[grep("^Time", vars)])]
-  d[d$Timestamp %in% day$Timestamp,vars] <- day[,vars]
+  vars <- vars[!(vars %in% grep("^Time", vars, value = TRUE))]
+  d[as.character(d$Timestamp) %in% as.character(day$Timestamp),vars] <- day[,vars]
 }
 proc.time() - ptm # approximately 48 mins
 
@@ -94,8 +101,8 @@ proc.time() - ptm # approximately 48 mins
 ##########
 
 ### only run this segment of code if there are particular variables you would like removed from the dataset ###
-### some preprocessing was done on this csv manually to flag variables for removal, refer to DataToBeExcluded file in repo ###
 ### variable names included in the DataToBeExcluded file, will be removed from the data in the code below ###
+### refer to DataToBeExcluded.csv from the user_updates folder in the repo ###
 
 remove <- read.csv(paste0(userUpdatesWD, "/DataToBeExcluded.csv"), header = TRUE, stringsAsFactors = FALSE)
 remove$Channel <- gsub("[[:punct:]]", "", remove$Channel)
@@ -112,6 +119,8 @@ for(i in 1:length(remove)){
 # Rename variables for interpretability
 ##########
 
+### refer to newVariableNames.csv from the user_updates folder in the repo ###
+
 # manual override of channel names
 rename <- read.csv(paste0(userUpdatesWD, "/newVariableNames.csv"), header = TRUE, stringsAsFactors = FALSE)
 updatedNames <- setNames(rename$NewDataLabel, rename$OldDataLabel)
@@ -123,8 +132,6 @@ names(d)[names(d) %in% rename$OldDataLabel] <- updatedNames[names(d)[names(d) %i
 # More time variable processing
 ##########
 
-# Remove extraneous timestamp variables, like TimeStamp_GPSTime, TimeStamp_SystemTime, TimeStamp_SchedulerTime
-d <- d[,!(names(d) %in% c("TimeStamp_GPSTime", "TimeStamp_SystemTime", "TimeStamp_SchedulerTime"))] 
 # create variable for day of week
 d$DayOfWeek <- as.factor(weekdays(d$Timestamp))
 # update timestamp count
@@ -145,7 +152,6 @@ subsystems <- unique(metadata$subsystem[!is.na(metadata$subsystem)])
 
 # import data dictionary
 dic <- read.csv(paste0(userUpdatesWD, "/dataDictionary.csv"), header = TRUE, stringsAsFactors = FALSE, na.strings = c("", NA, " "))
-dic <- na.omit(dic)
 
 # Pull variable descriptions, measurement location, measured parameter and units from data dictionary
 metadata <- merge(metadata, dic[,c("Data.Label", "Measurement.Location", "Measured.Parameter", "Description", "Units")], by.x = 'variable', by.y = 'Data.Label', all.x = TRUE)
@@ -158,27 +164,92 @@ metadata$Units[grep("_Status", metadata$Variable)] <- "Binary Status"
 metadata$max_value <- 0
 metadata$min_value <- 0
 for(col in names(d)){
-  metadata$max_value[metadata$Variable == col] <- round(max(as.numeric(d[,col])), digits = 4)
-  metadata$min_value[metadata$Variable == col] <- round(min(as.numeric(d[,col])), digits = 4)
+  metadata$max_value[metadata$Variable == col] <- round(max(as.numeric(d[,col]), na.rm = TRUE), digits = 4)
+  metadata$min_value[metadata$Variable == col] <- round(min(as.numeric(d[,col]), na.rm = TRUE), digits = 4)
 }
 
 ###############
-# write final CSVs to processed folder
+# write minute-level CSVs to processed folder
 ###############
 
 # write entire dataset
-write.csv(d, file = paste0(processedDataWD, "/data.csv"), row.names = FALSE)
+write.csv(d, file = paste0(processedDataWD, "/data-minute.csv"), row.names = FALSE)
 
 # write CSV for each subsystem
 for(subsystem in subsystems){
   vars <- c(timeVars, names(d)[grep(paste0("^",subsystem,"_"), names(d))])
   data_subsystem <- d[,vars]
-  write.csv(data_subsystem, file = paste0(processedDataWD, "/", subsystem, ".csv"), row.names = FALSE)
+  write.csv(data_subsystem, file = paste0(processedDataWD, "/", subsystem, "-minute.csv"), row.names = FALSE)
 }
+
+proc.time() - ptm #approximately 78 mins
+
+###############
+# Create hourly-aggregates
+###############
+
+### refer to HourlyDataConversions.csv from the user_updates folder in the repo ###
+
+# manual override of channel names
+hourly <- read.csv(paste0(userUpdatesWD, "/HourlyDataConversions.csv"), header = TRUE, stringsAsFactors = FALSE)
+useAvg <- hourly$Data.Label[hourly$Hourly.conversion == "Average"]
+useValueAtHour <- hourly$Data.Label[hourly$Hourly.conversion == "Hour value"]
+useSum <- hourly$Data.Label[hourly$Hourly.conversion == "Sum"]
+
+# create dataframe to store hourly aggregates
+d_hour <- data.frame(Timestamp = seq(ISOdatetime(2015, 02, 01, 0, 0, 0, tz = "EST"), ISOdatetime(2016, 01, 31, 0, 0, 0, tz = "EST"), "hour"))
+temp <- as.data.frame(matrix(NA, nrow = nrow(d_hour), ncol = length(names(d)[!(names(d) %in% timeVars)])))
+names(temp) <- names(d)[!(names(d) %in% timeVars)]
+d_hour <- cbind(d_hour, temp)
+
+# populate the aggregates based on average of values in the hour
+for(hour in d_hour$Timestamp){
+  temp <- apply(d[(d$Timestamp >= hour) & (d$Timestamp < (hour + 60*60)), useAvg], 
+                2, 
+                function(x) ifelse(length(x[is.na(x)]) == length(x), NA, mean(x, na.rm = TRUE)))
+  d_hour[d_hour$Timestamp == hour,useAvg] <- temp
+}
+
+# populate the aggregates based on max value (value at the end of the hour)
+for(hour in d_hour$Timestamp){
+  temp <- apply(d[(d$Timestamp >= hour) & (d$Timestamp < (hour + 60*60)), useValueAtHour], 
+                                                           2, 
+                                                           function(x) ifelse(length(x[is.na(x)]) == length(x), NA, max(x, na.rm = TRUE)))
+  d_hour[d_hour$Timestamp == hour,useValueAtHour] <- temp
+}
+
+# populate the aggregate (only one variable) based on sum of values across the hour
+for(hour in d_hour$Timestamp){
+  temp <- sum(d[(d$Timestamp >= hour) & (d$Timestamp < (hour + 60*60)), useSum], na.rm = TRUE)
+  d_hour[d_hour$Timestamp == hour, useSum] <- temp
+}
+
+# update metadata file with aggregation method used
+metadata$Aggregation_Method[metadata$Variable %in% useAvg] <- "Average"
+metadata$Aggregation_Method[metadata$Variable %in% useSum] <- "Sum"
+metadata$Aggregation_Method[metadata$Variable %in% useValueAtHour] <- "Hour value"
+
+###############
+# write hourly-aggregated CSVs to processed folder
+###############
+
+# write entire dataset
+write.csv(d_hour, file = paste0(processedDataWD, "/data-hour.csv"), row.names = FALSE)
+
+# write CSV for each subsystem
+for(subsystem in subsystems){
+  vars <- c("Timestamp", names(d_hour)[grep(paste0("^",subsystem,"_"), names(d_hour))])
+  data_subsystem <- d_hour[,vars]
+  write.csv(data_subsystem, file = paste0(processedDataWD, "/", subsystem, "-hour.csv"), row.names = FALSE)
+}
+
+proc.time() - ptm
+
+###############
+# Write metadata file
+###############
 
 # write metadata file
 write.csv(metadata, file = paste0(processedDataWD, "/metadata.csv"), row.names = FALSE)
 
-proc.time() - ptm #approximately 78 mins
-
-#save(d, data_subsystem, day, dic, metadata, remove, altSubSystem, channel, mainWD, naics, path, paths, processedDataWD, rawDataWD, rawDataAltSystemWD, subsystem, subsystems, timeVars, vars, file = '/Users/prioberoi/Documents/nist/netZero/internal/cleaning_script_2016-10-18.Rda')
+#save(d, d_hour, data_subsystem, day, dic, metadata, remove, altSubSystem, mainWD, path, paths, processedDataWD, rawDataWD, rawDataAltSystemWD, subsystem, subsystems, timeVars, vars, file = '/Users/prioberoi/Documents/nist/netZero/internal/cleaning_script_2016-10-18.Rda')
